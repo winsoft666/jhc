cc52e4bbd46a8d84047f7586db2582f379c0652e
winsoft666 <winsoft666@outlook.com>
2022-07-04 12:37:07 +0800

    Add version macro

--- include/jhc/config.hpp
+++ include/jhc/config.hpp
@@ -2,12 +2,16 @@
 #define JHC_CONFIG_HPP__
 #pragma once
 
-#undef JHC_NOT_HEADER_ONLY
+/* #undef JHC_NOT_HEADER_ONLY */
 
 #ifdef JHC_NOT_HEADER_ONLY
 #define JHC_INLINE
 #else
 #define JHC_INLINE inline
 #endif
+
+#define JHC_VERSION_MAJOR 1
+#define JHC_VERSION_MINOR 0
+#define JHC_VERSION_PATCH 1
 
 #endif //!JHC_CONFIG_HPP__
